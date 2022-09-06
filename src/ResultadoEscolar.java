c9b13b82153a56376befa7624d2ee446108856b7
J.Stefani <jota.stefani@hotmail.com>
2022-09-06 19:20:59 -0300

    Third commit

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,7 +1,7 @@
 public class ResultadoEscolar {
   public static void main(String[] args) {
-    int nota = 6;
-    String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+    int nota = 4;
+    String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota <7 ? "Recuperação" : "Reprovado";
     
     System.out.println(resultado);
   }
