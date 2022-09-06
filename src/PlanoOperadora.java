130ae0166c2aae496b7cd4b0bb5ad52acf9f6256
J.Stefani <jota.stefani@hotmail.com>
2022-09-06 19:49:16 -0300

    Fiveth

---
+++ src/PlanoOperadora.java
@@ -0,0 +1,19 @@
+public class PlanoOperadora {
+
+  public static void main(String[] args) {
+    String plano = "P";
+
+    switch (plano) {
+      case "T": {
+        System.out.println("5GB outube");
+      }
+      case "M": {
+        System.out.println("Whatsapp e Instagram grátis");
+      }
+      case "P": {
+        System.out.println("100 Minutos de ligações");
+      }
+    }
+  }
+}
+
