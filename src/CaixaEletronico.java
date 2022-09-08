a1c6b5664fbab17bd1dbbde65056dce5c3403c9c
J.Stefani <jota.stefani@hotmail.com>
2022-09-08 09:19:41 -0300

    EstruturasRepetição

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,7 +1,7 @@
 public class CaixaEletronico {
   public static void main(String[] args) {
     double saldo = 25.0;
-    double valorSolicitado = 27.0;
+    double valorSolicitado = 17.0;
 
     if(valorSolicitado < saldo){
       saldo = saldo - valorSolicitado;
