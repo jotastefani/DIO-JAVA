7dbcad8d34337cbceb36fd49f2c5e224da493d24
J.Stefani <jota.stefani@hotmail.com>
2022-09-10 15:43:03 -0300

    Try/Catch

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,8 +1,11 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
   public static void main(String[] args) {
+
+    try {
     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
     System.out.println("Digite seu nome");
@@ -22,5 +25,8 @@ public class AboutMe {
     System.out.println("Tenho " + idade + " anos ");
     System.out.println("Minha altura é " + altura + "cm ");
     scanner.close();
+    } catch (InputMismatchException e){
+      System.out.println("O campo idade precisa ser em anos inteiros e o campo altura em centímetros");
+    }
   }
 }
